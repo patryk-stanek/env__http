9f48dbac26cb6910a5cac3e7daf27b0204d4b9a3
Patryk Stanek <p.stanek3@gmail.com>
2019-03-21 17:33:18 +0100

    minor fixes

--- index.js
+++ index.js
@@ -2,17 +2,7 @@ var http = require('http');
 var fs = require('fs');
 var server = http.createServer();
 
-var content;
-
 server.on('request', function (request, response) {
-    // response.write('<body>');
-    // response.write('<h1>This is awesome!</h1>')
-    // response.write('</body>');
-    // response.end();
-    //po wywołaniu response.end nie możemy zapisywać niczego do strumienia, to zwróci błąd
-    // response.write('<h2>This is less awesome!</h2>');
-
-    //odpowiedzi wymagaja kodowania aby przegladarka wiedziala w jaki sposob odczytac wiadomosc
     if (request.method === 'GET' && request.url === '/') {
         fs.readFile('./index.html', 'utf-8', function(err, data) {
             if (err) throw err;
@@ -29,7 +19,7 @@ server.on('request', function (request, response) {
             response.write(data);
             response.end();
         });
-    };
+    }
 });
 
 server.listen(9000);
